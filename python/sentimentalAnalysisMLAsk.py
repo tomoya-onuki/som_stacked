c24f552025a3b0af04cf0c71fb7fe1488cc5a9a5
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-12-29 18:27:18 +0900

    1st deploy

--- python/sentimentalAnalysisMLAsk.py
+++
@@ -1,9 +0,0 @@
-import sys
-import path
-sys.path.append("/Users/tomo/opt/miniconda3/lib/python3.9/site-packages (0.3.2)")
-for p in sys.path:
-    print(p)
-
-from mlask import MLAsk
-emotion_analyzer = MLAsk()
-emotion_analyzer.analyze('こんな朝にはただただ世界が好きでたまらないという気がしない？')
